e75cfc4faac63d5c59273c4f854ad05526cb79b4
Rezi Apriliansyah <Reziapriliansyah@gmail.com>
2017-10-10 09:41:18 +0700

    navbar hotel

---
+++ footer.php
@@ -0,0 +1,273 @@
+    <footer class="revealed">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-3 col-sm-3">
+                    <h3>Visit Palembang</h3>
+                    <ul>
+                        <li><a href="#" >See & Do</a></li>
+                        <li><a href="#" >Events</a></li>
+                        <li><a href="#" >Kuliner</a></li>
+                        <li><a href="#" >Lifestyle</a></li>
+                        <li><a href="#" >Penginapan</a></li>
+                        <li><a href="#" >Transportasi</a></li>
+                        <li><a href="#" >Panduan Wisata</a></li>
+                        <li><a href="#" >Informasi</a></li>
+                    </ul>
+                </div>
+                <div class="col-md-3 col-sm-3">
+                    <h3>About</h3>
+                    <ul>
+                        <li><a href="#">About us</a></li>
+                        <li><a href="#">FAQ</a></li>
+                        <li><a href="#">Login</a></li>
+                        <li><a href="#">Register</a></li>
+                         <li><a href="#">Terms and condition</a></li>
+                    </ul>
+                </div>
+                <div class="col-md-3 col-sm-3">
+                    <h3>Discover</h3>
+                    <ul>
+                        <li><a href="#">Community blog</a></li>
+                        <li><a href="#">Tour guide</a></li>
+                        <li><a href="#">Wishlist</a></li>
+                         <li><a href="#">Gallery</a></li>
+                    </ul>
+                </div>
+                <div class="col-md-3 col-sm-3">
+                    <h3>Need help?</h3>
+                    <a href="tel://004542344599" id="phone">+628 96 9255 7552</a>
+                    <a href="mailto:hallo@visitpalembang.id" id="email_footer">hallo@visitpalembang.id</a>
+                </div>
+            </div><!-- End row -->
+            <hr><!-- 
+            <div class="row">
+                <div class="col-md-12">
+                    <h3>Pernah Diliput Oleh : </h3>
+
+                </div>
+            </div> -->
+            <div class="row">
+                <div class="col-md-12">
+                    <div id="social_footer">
+                        <ul>
+                            <li><a href="#"><i class="icon-facebook"></i></a></li>
+                            <li><a href="#"><i class="icon-instagram"></i></a></li>
+                            <li><a href="#"><i class="icon-youtube-play"></i></a></li>
+                        </ul>
+                        <p>© Visit Palembang 2017</p>
+                    </div>
+                </div>
+            </div><!-- End row -->
+        </div><!-- End container -->
+    </footer><!-- End footer -->
+
+    <div id="toTop"></div><!-- Back to top button -->
+	
+	<!-- Search Menu -->
+	<div class="search-overlay-menu">
+		<span class="search-overlay-close"><i class="icon_set_1_icon-77"></i></span>
+		<form role="search" id="searchform" method="get">
+			<input value="" name="q" type="search" placeholder="Search..." />
+			<button type="submit"><i class="icon_set_1_icon-78"></i>
+			</button>
+		</form>
+	</div><!-- End Search Menu -->
+
+    <!-- Common scripts -->
+    <script src="js/jquery.min.js"></script>
+    <script src="js/common_scripts_min.js"></script>
+    <script src="js/functions.js"></script>
+
+	<!-- Specific scripts -->
+	<!-- Check and radio inputs -->
+	<script src="js/icheck.js"></script>
+	<script>
+		$('input').iCheck({
+			checkboxClass: 'icheckbox_square-grey',
+			radioClass: 'iradio_square-grey'
+		});
+	</script>
+	<!-- Map -->
+
+        
+    <!-- SLIDER REVOLUTION SCRIPTS  -->
+    <script type="text/javascript" src="rev-slider-files/js/jquery.themepunch.tools.min.js"></script>
+	<script type="text/javascript" src="rev-slider-files/js/jquery.themepunch.revolution.min.js"></script>
+	<script type="text/javascript" src="rev-slider-files/js/extensions/revolution.extension.actions.min.js"></script>
+	<script type="text/javascript" src="rev-slider-files/js/extensions/revolution.extension.carousel.min.js"></script>
+	<script type="text/javascript" src="rev-slider-files/js/extensions/revolution.extension.kenburn.min.js"></script>
+	<script type="text/javascript" src="rev-slider-files/js/extensions/revolution.extension.layeranimation.min.js"></script>
+	<script type="text/javascript" src="rev-slider-files/js/extensions/revolution.extension.migration.min.js"></script>
+	<script type="text/javascript" src="rev-slider-files/js/extensions/revolution.extension.navigation.min.js"></script>
+	<script type="text/javascript" src="rev-slider-files/js/extensions/revolution.extension.parallax.min.js"></script>
+	<script type="text/javascript" src="rev-slider-files/js/extensions/revolution.extension.slideanims.min.js"></script>
+	<script type="text/javascript" src="rev-slider-files/js/extensions/revolution.extension.video.min.js"></script>
+	<script type="text/javascript">
+		var tpj = jQuery;
+
+		var revapi54;
+		tpj(document).ready(function () {
+			if (tpj("#rev_slider_54_1").revolution == undefined) {
+				revslider_showDoubleJqueryError("#rev_slider_54_1");
+			} else {
+				revapi54 = tpj("#rev_slider_54_1").show().revolution({
+					sliderType: "standard",
+					jsFileLocation: "rev-slider-files/js/",
+					sliderLayout: "fullwidth",
+					dottedOverlay: "none",
+					delay: 9000,
+					navigation: {
+							keyboardNavigation:"off",
+							keyboard_direction: "horizontal",
+							mouseScrollNavigation:"off",
+                             mouseScrollReverse:"default",
+							onHoverStop:"off",
+							touch:{
+								touchenabled:"on",
+								touchOnDesktop:"off",
+								swipe_threshold: 75,
+								swipe_min_touches: 50,
+								swipe_direction: "horizontal",
+								drag_block_vertical: false
+							}
+							,
+							arrows: {
+								style:"uranus",
+								enable:true,
+								hide_onmobile:true,
+								hide_under:778,
+								hide_onleave:true,
+								hide_delay:200,
+								hide_delay_mobile:1200,
+								tmp:'',
+								left: {
+									h_align:"left",
+									v_align:"center",
+									h_offset:20,
+                                    v_offset:0
+								},
+								right: {
+									h_align:"right",
+									v_align:"center",
+									h_offset:20,
+                                    v_offset:0
+								}
+							}
+						},
+					responsiveLevels: [1240, 1024, 778, 480],
+					visibilityLevels: [1240, 1024, 778, 480],
+					gridwidth: [1240, 1024, 778, 480],
+					gridheight: [700, 550, 860, 480],
+					lazyType: "none",
+					parallax: {
+						type: "mouse",
+						origo: "slidercenter",
+						speed: 2000,
+						levels: [2, 3, 4, 5, 6, 7, 12, 16, 10, 50, 47, 48, 49, 50, 51, 55],
+						disable_onmobile: "on"
+					},
+					shadow: 0,
+					spinner: "off",
+					stopLoop: "on",
+					stopAfterLoops: 0,
+					stopAtSlide: 1,
+					shuffle: "off",
+					autoHeight: "off",
+					disableProgressBar: "on",
+					hideThumbsOnMobile: "off",
+					hideSliderAtLimit: 0,
+					hideCaptionAtLimit: 0,
+					hideAllCaptionAtLilmit: 0,
+					debugMode: false,
+					fallbacks: {
+						simplifyAll: "off",
+						nextSlideOnWindowFocus: "off",
+						disableFocusListener: false,
+					}
+				});
+			}
+		}); /*ready*/
+	</script>
+	
+	<script src="js/notify_func.js"></script>
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCUSK3JydXzrDD_eiBdd4sGnnZui-JYte8&callback=initMap">
+    </script>
+    <script src="js/icheck.js"></script>
+	<script>
+		$('input').iCheck({
+			checkboxClass: 'icheckbox_square-grey',
+			radioClass: 'iradio_square-grey'
+		});
+	</script>
+	<!-- Date and time pickers -->
+	<script src="js/jquery.sliderPro.min.js"></script>
+	<script type="text/javascript">
+		$(document).ready(function ($) {
+			$('#Img_carousel').sliderPro({
+				width: 960,
+				height: 500,
+				fade: true,
+				arrows: true,
+				buttons: false,
+				fullScreen: false,
+				smallSize: 500,
+				startSlide: 0,
+				mediumSize: 1000,
+				largeSize: 3000,
+				thumbnailArrows: true,
+				autoplay: false
+			});
+		});
+	</script>
+
+
+	<!-- Date and time pickers -->
+	<script src="js/bootstrap-datepicker.js"></script>
+	<script>
+		$('input.date-pick').datepicker('setDate', 'today');
+	</script>
+	<!-- Map -->
+	<script src="http://maps.googleapis.com/maps/api/js"></script>
+	<script src="js/map.js"></script>
+	<!-- Carousel -->
+	<script src="js/owl.carousel.min.js"></script>
+	<script>
+		$(document).ready(function () {
+			$(".carousel").owlCarousel({
+				items: 4,
+				itemsDesktop: [1199, 3],
+				itemsDesktopSmall: [979, 3]
+			});
+		});
+	</script>
+
+	<!--Review modal validation -->
+	<script src="assets/validate.js"></script>
+    <!-- <script src="http://maps.googleapis.com/maps/api/js"></script> -->
+    <script type="text/javascript" src="js/map_home.js"></script>
+    <script type="text/javascript" src="js/infobox.js"></script>
+    <script type="text/javascript" src="ayoshare/ayoshare.js"></script>
+    <script type="text/javascript">
+        $(function() {
+            $(".share").ayoshare({
+                button: {
+                    google : true,
+                    stumbleupon : false,
+                    facebook : true,
+                    pinterest : true,
+                    bufferapp : false,
+                    reddit : false,
+                    vk : false,
+                    pocket : false,
+                    twitter : true,
+                    digg : false,
+                    telegram : true,
+                    sms : false
+                }
+            });
+            
+        });
+    </script>  
+</body>
+
+</html>
---
+++ header.php
@@ -0,0 +1,272 @@
+<!DOCTYPE html>
+<!--[if IE 8]><html class="ie ie8"> <![endif]-->
+<!--[if IE 9]><html class="ie ie9"> <![endif]-->
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+    <meta name="description" content="Citytours - Premium site template for city tours agencies, transfers and tickets.">
+    <meta name="author" content="Ansonika">
+    <title>Visit Palembang</title>
+
+    <!-- Favicons-->
+    <link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon">
+    <link rel="apple-touch-icon" type="image/x-icon" href="img/apple-touch-icon-57x57-precomposed.png">
+    <link rel="apple-touch-icon" type="image/x-icon" sizes="72x72" href="img/apple-touch-icon-72x72-precomposed.png">
+    <link rel="apple-touch-icon" type="image/x-icon" sizes="114x114" href="img/apple-touch-icon-114x114-precomposed.png">
+    <link rel="apple-touch-icon" type="image/x-icon" sizes="144x144" href="img/apple-touch-icon-144x144-precomposed.png">
+	<link href="//cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+	<!-- Google web fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Gochi+Hand|Lato:300,400|Montserrat:400,400i,700,700i" rel="stylesheet">
+
+    <!-- BASE CSS -->
+    <link href="css/base.css" rel="stylesheet">
+
+    <!-- REVOLUTION SLIDER CSS -->
+    <link rel="stylesheet" type="text/css" href="rev-slider-files/fonts/pe-icon-7-stroke/css/pe-icon-7-stroke.css">
+	<link rel="stylesheet" type="text/css" href="rev-slider-files/fonts/font-awesome/css/font-awesome.css">
+    <link rel="stylesheet" type="text/css" href="rev-slider-files/css/settings.css">
+    <link href="ayoshare/ayoshare.css" rel="stylesheet">
+    <!-- Radio and check inputs -->
+    <link href="css/skins/square/grey.css" rel="stylesheet">
+    
+    <link href="css/slider-pro.min.css" rel="stylesheet">
+    <link href="css/date_time_picker.css" rel="stylesheet">
+    <link href="css/owl.carousel.css" rel="stylesheet">
+    <link href="css/owl.theme.css" rel="stylesheet">
+    <!-- Range slider -->
+    <link href="css/ion.rangeSlider.css" rel="stylesheet">
+    <link href="css/ion.rangeSlider.skinFlat.css" rel="stylesheet">
+
+	<style>
+		.tp-caption.NotGeneric-Title,
+		.NotGeneric-Title {
+			color: rgba(255, 255, 255, 1.00);
+			font-size: 70px;
+			line-height: 70px;
+			font-weight: 800;
+			font-style: normal;
+			text-decoration: none;
+			background-color: transparent;
+			border-color: transparent;
+			border-style: none;
+			border-width: 0px;
+			border-radius: 0 0 0 0px
+		}
+
+		.tp-caption.NotGeneric-SubTitle,
+		.NotGeneric-SubTitle {
+			color: rgba(255, 255, 255, 1.00);
+			font-size: 13px;
+			line-height: 20px;
+			font-weight: 500;
+			font-style: normal;
+			text-decoration: none;
+			background-color: transparent;
+			border-color: transparent;
+			border-style: none;
+			border-width: 0px;
+			border-radius: 0 0 0 0px;
+			letter-spacing: 4px
+		}
+
+		.tp-caption.NotGeneric-Icon,
+		.NotGeneric-Icon {
+			color: rgba(255, 255, 255, 1.00);
+			font-size: 30px;
+			line-height: 30px;
+			font-weight: 400;
+			font-style: normal;
+			text-decoration: none;
+			background-color: rgba(0, 0, 0, 0);
+			border-color: rgba(255, 255, 255, 0);
+			border-style: solid;
+			border-width: 0px;
+			border-radius: 0px 0px 0px 0px;
+			letter-spacing: 3px
+		}
+
+		.tp-caption.NotGeneric-Button,
+		.NotGeneric-Button {
+			color: rgba(255, 255, 255, 1.00);
+			font-size: 14px;
+			line-height: 14px;
+			font-weight: 500;
+			font-style: normal;
+			text-decoration: none;
+			background-color: rgba(0, 0, 0, 0);
+			border-color: rgba(255, 255, 255, 0.50);
+			border-style: solid;
+			border-width: 1px;
+			border-radius: 0px 0px 0px 0px;
+			letter-spacing: 3px
+		}
+
+		.tp-caption.NotGeneric-Button:hover,
+		.NotGeneric-Button:hover {
+			color: rgba(255, 255, 255, 1.00);
+			text-decoration: none;
+			background-color: transparent;
+			border-color: rgba(255, 255, 255, 1.00);
+			border-style: solid;
+			border-width: 1px;
+			border-radius: 0px 0px 0px 0px;
+			cursor: pointer
+		}
+	</style>
+
+    <!--[if lt IE 9]>
+      <script src="js/html5shiv.min.js"></script>
+      <script src="js/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body>
+
+<!--[if lte IE 8]>
+    <p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a>.</p>
+<![endif]-->
+
+    <div id="preloader">
+        <div class="sk-spinner sk-spinner-wave">
+            <div class="sk-rect1"></div>
+            <div class="sk-rect2"></div>
+            <div class="sk-rect3"></div>
+            <div class="sk-rect4"></div>
+            <div class="sk-rect5"></div>
+        </div>
+    </div>
+    <!-- End Preload -->
+
+    <div class="layer"></div>
+    <!-- Mobile menu overlay mask -->
+
+    <!-- Header================================================== -->
+    <header>
+        <div id="top_line">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-6 col-sm-6 col-xs-6"><i class="icon-phone"></i><strong>+628 xxxxxxxx</strong></div>
+                    
+                    <div class="col-md-6 col-sm-6 col-xs-6">
+                        <ul id="top_links">
+                            <li>
+                                <div class="dropdown dropdown-access">
+                                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" id="access_link">Sign in</a>
+                                    <div class="dropdown-menu">
+                                        <div class="row">
+                                            <div class="col-md-6 col-sm-6 col-xs-6">
+                                                <a href="#" class="btn btn-primary">
+                                                    <i class="icon-facebook"></i>Facebook </a>
+                                            </div>
+                                            <div class="col-md-6 col-sm-6 col-xs-6">
+                                                <a href="#" class="btn btn-danger">
+                                                    <i class="fa fa-google"></i> Google </a>
+                                            </div>
+                                        </div>
+                                        <div class="login-or">
+                                            <hr class="hr-or">
+                                            <span class="span-or">or</span>
+                                        </div>
+                                        <div class="form-group">
+                                            <input type="text" class="form-control" id="inputUsernameEmail" placeholder="Email">
+                                        </div>
+                                        <div class="form-group">
+                                            <input type="password" class="form-control" id="inputPassword" placeholder="Password">
+                                        </div>
+                                        <a id="forgot_pw" href="#">Forgot password?</a>
+                                        <input type="submit" name="Sign_in" value="Sign in" id="Sign_in" class="button_drop">
+                                        <input type="submit" name="Sign_up" value="Sign up" id="Sign_up" class="button_drop outline">
+                                    </div>
+                                </div><!-- End Dropdown access -->
+                            </li>
+                        </ul>
+                    </div>
+                </div><!-- End row -->
+            </div><!-- End container-->
+        </div><!-- End top line-->
+        
+        <div class="container">
+            <div class="row">
+                <div class="col-md-3 col-sm-3 col-xs-3">
+                    <div id="logo_home">
+                    	<h1><a href="index.html" title="City tours travel template">Kujungi Palembang</a></h1>
+                    </div>
+                </div>
+                <nav class="col-md-9 col-sm-9 col-xs-9">
+                    <a class="cmn-toggle-switch cmn-toggle-switch__htx open_close" href="javascript:void(0);"><span>Menu mobile</span></a>
+                    <div class="main-menu">
+                        <div id="header_menu">
+                            <img src="img/logo_sticky.png" width="160" height="34" alt="City tours" data-retina="true">
+                        </div>
+                        <a href="#" class="open_close" id="close_in"><i class="icon_set_1_icon-77"></i></a>
+                        <ul>
+                            <li><a href="#">Home</a></li>
+                            <li class="megamenu submenu">
+                                <a href="javascript:void(0);" class="show-submenu-mega">See & Do<i class="icon-down-open-mini"></i></a>
+                                <div class="menu-wrapper">
+                                    <div class="col-md-4">
+                                        <h3>Pages</h3>
+                                        <ul>
+                                            <li><a href="#">Daerah Sekitar</a></li>
+                                            <li><a href="all_tours_grid.html">Seni</a></li>
+                                            <li><a href="all_tours_map_listing.html">Sejarah</a></li>
+                                            <li><a href="single_tour.html">Arsitektur</a></li>
+                                            <li><a href="single_tour_with_gallery.html">Budaya</a></li>
+                                            <li><a href="single_tour_working_booking.php">Rekreasi dan Bersantai</a></li>
+                                            <li><a href="cart.html">Alam dan Margasatwa</a></li>
+                                            <li><a href="payment.html">Melalui Palembang</a></li>
+                                        </ul>
+                                    </div>
+                                    <div class="col-md-8">
+                                        <h3>Artikel</h3>
+
+                                        <div class="row">
+                                            <div class="col-md-4">
+                                                <p>
+                                                <img src="img/bus.jpg" alt="Pic" class="img img-responsive">
+                                                <h4><span>Sightseen tour</span> booking</h4>
+                                                </p>
+                                            </div>
+                                            <div class="col-md-4">
+                                                <p>
+                                                <img src="img/bus.jpg" alt="Pic" class="img img-responsive">
+                                                <h4><span>Sightseen tour</span> booking</h4>
+                                                </p>
+                                            </div>
+                                            <div class="col-md-4">
+                                                <p>
+                                                <img src="img/bus.jpg" alt="Pic" class="img img-responsive">
+                                                <h4><span>Sightseen tour</span> booking</h4>
+                                                </p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </li>
+                            <li><a href="#">Events</a></li>
+                            <li><a href="#">Kuliner</a></li>
+                            <li><a href="#">Lifestyle</a></li>
+                            <li><a href="#">Penginapan</a></li>
+                            <li><a href="#">Transportasi</a></li>
+                            <li><a href="#">Panduan WIsata</a></li>
+                            <li><a href="#">Informasi</a></li>
+                            
+                        </ul>
+                    </div><!-- End main-menu -->
+                    <ul id="top_tools">
+                        <li>
+                            <div class="dropdown dropdown-search">
+                                <a href="#" class="search-overlay-menu-btn" data-toggle="dropdown"><i class="icon-search"></i></a>
+                            </div>
+                        </li>
+                    </ul>
+                </nav>
+            </div>
+        </div><!-- container -->
+    </header><!-- End Header -->
+	
